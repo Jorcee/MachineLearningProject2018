073822a2ad1fe33a6762dd21d045d2eba83ee862
Jordan Goldstein <JordanGoldstein@gatech.edu>
2018-11-28 22:51:45 +0800

    Made abstract better

--- clf_abstract.py
+++ clf_abstract.py
@@ -1,22 +1,37 @@
 import random
 from gensim.utils import simple_preprocess
 from gensim.models.keyedvectors import Doc2VecKeyedVectors
+from gensim.models import Doc2Vec
+from scipy import spatial
 class clf_abstract: 
     def __init__(self):
         self.doc_vector = Doc2VecKeyedVectors.load('doc2vec.kv')
+        self.doc_model = Doc2Vec.load('doc2vec.model')
 
     def train(self,data,label):
         print('clf_abstract train start')
         pass
         print('clf_abstract train end')
-    #expects paper1 and paper2 to be loaded json documents
+    #expects paper1 and paper2 to be vectors fro the abstracts of those papers
     def predict(self,paper1, paper2):
-        ab1 = simple_preprocess(paper1['abstract'])
-        ab2 = simple_preprocess(paper1['abstract'])
+        return 1 - spatial.distance.cosine(paper1, paper2)
+
+        #The old, crappy way
+        # ab1 = simple_preprocess(paper1['abstract'])
+        # ab2 = simple_preprocess(paper1['abstract'])
+        # try:
+        #     sim = self.doc_vector.n_similarity(ab1,ab2)
+        # except Exception as e:
+        #     print(e)
+        #     return 0
+        # return sim
+
+    #Returns the vector of a paper's abstract
+    def get_vector(self, paper):
+        doc = simple_preprocess(paper['abstract'])
         try:
-            sim = self.doc_vector.n_similarity(ab1,ab2)
+            return self.doc_model.infer_vector(doc)
         except Exception:
-            return 0
-        return sim
+            return [0]
 
 
